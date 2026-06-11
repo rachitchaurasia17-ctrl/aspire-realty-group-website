d79eed4b9e20f727e391fc785088e3d1a7e15d00
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:34:42 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for ASPIRE REA
 
 Firm facts:
 - Firm: ASPIRE REALTY GROUP
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: SCO 14, First Floor, VIP Road Commercial Hub, Zirakpur, Punjab 140603
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
